98fc42de7c79f00b576c36597a5ca965b1bb1320
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-11-20 09:50:00 +0700

    Upload project exercise

---
+++ threading-fundamentals/src/main/java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread2 = new Thread(() -> {
+            for(int i=0;i<20 ;i++){
+                System.out.println("We are in thread: " + Thread.currentThread().getName());
+            }
+
+        });
+        thread2.setPriority(Thread.MIN_PRIORITY);
+        thread2.setName("xxx");
+
+        Thread thread = new Thread(() -> {
+            // code that will run in a new thread
+            for(int i=0;i<20 ;i++){
+                System.out.println("We are in thread: " + Thread.currentThread().getName());
+            }
+        });
+        thread.setName("new worker thread");
+        thread.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("We are in thread: " + Thread.currentThread().getName()+" before starting a new thread");
+        thread2.start();
+        thread.start();
+        System.out.println("We are in thread: " + Thread.currentThread().getName()+" after starting a new thread");
+        Thread.sleep(10000);
+
+
+    }
+}
