b4c75ed7ae71d1f6b16c49889dac7f89bed23c73
Trần Vũ Trường <truongtv1399it@gmail.com>
2022-11-21 07:08:47 +0700

    Up source code dataSharing

--- threading-fundamentals/src/main/java/Main.java
+++ threading-fundamentals/src/main/java/Main.java
@@ -1,3 +1,7 @@
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
         Thread thread2 = new Thread(() -> {
@@ -17,12 +21,25 @@ public class Main {
         });
         thread.setName("new worker thread");
         thread.setPriority(Thread.MAX_PRIORITY);
+
+        Thread thread3 = new Thread(() -> {
+            // code that will run in a new thread
+           throw new RuntimeException("intentional exception");
+        });
+        thread3.setName("new worker thread");
+        thread3.setPriority(Thread.NORM_PRIORITY);
+
+        thread3.setUncaughtExceptionHandler((t, e) -> {
+            System.out.println("A critical error happend in thread " + t.getName() + " the error is " + e.getMessage());
+        });
+
+
         System.out.println("We are in thread: " + Thread.currentThread().getName()+" before starting a new thread");
         thread2.start();
         thread.start();
+        thread3.start();
         System.out.println("We are in thread: " + Thread.currentThread().getName()+" after starting a new thread");
         Thread.sleep(10000);
-
 
     }
 }
---
+++ threading-fundamentals/src/main/java/criticalandsynchronization/Main.java
@@ -0,0 +1,77 @@
+package criticalandsynchronization;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        InventoryCounter inventoryCounter = new InventoryCounter();
+        IncrementingThread incrementingThread = new IncrementingThread(inventoryCounter);
+        DecrementingThread decrementingThread = new DecrementingThread(inventoryCounter);
+
+        Thread thread1 = new Thread(incrementingThread);
+
+        Thread thread2 = new Thread(decrementingThread);
+
+        thread1.setName("IncrementingThread");
+        thread2.setName("DecrementingThread");
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println("We currently have "+ inventoryCounter.getItems() + "items");
+    }
+
+    public static class DecrementingThread implements Runnable{
+        private InventoryCounter inventoryCounter;
+
+        public DecrementingThread(InventoryCounter inventoryCounter){
+            this.inventoryCounter = inventoryCounter;
+        }
+
+
+        @Override
+        public void run() {
+            for(int i=0 ;i<10000 ;i++){
+                inventoryCounter.decrement();
+            }
+        }
+    }
+
+    public static class IncrementingThread implements Runnable{
+        private InventoryCounter inventoryCounter;
+
+        public IncrementingThread(InventoryCounter inventoryCounter){
+            this.inventoryCounter = inventoryCounter;
+        }
+
+        @Override
+        public void run() {
+
+            for(int i=0 ;i<10000 ;i++){
+                inventoryCounter.increment();
+            }
+        }
+    }
+
+
+    private static class InventoryCounter{
+        private int items = 0;
+
+        public void increment(){
+            System.out.println(Thread.currentThread().getName());
+            items++;
+        }
+
+        public void decrement(){
+            System.out.println(Thread.currentThread().getName());
+            items--;
+        }
+
+        public int getItems(){
+            return items;
+        }
+
+    }
+
+}
